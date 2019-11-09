35b44d87d7c889f5c034b1476b5b4d74f50bb9a7
liu517321 <48541988+liu517321@users.noreply.github.com>
2019-11-09 15:17:00 +0800

    Add files via upload
    懒汉模式-多线程版-二次判断-性能高
    （volatile 的使用--只能出现在修饰变量中）

---
+++ SingletonLazyVersion3.java
@@ -0,0 +1,18 @@
+public class SingletonLazyVersion3 {
+    private SingletonLazyVersion3() {}
+    private volatile static SingletonLazyVersion3 instance = null;
+
+    // A
+    private static SingletonLazyVersion3 getInstance() {
+        if (instance == null) {
+            synchronized (SingletonLazyVersion3.class) {
+                if (instance == null) {
+                    instance = new SingletonLazyVersion3();
+                }
+            }
+        }
+
+        return instance;
+    }
+}
+
